69361cb21fd6fff09b92784a58917b2bb625b72d
wireless-anon <wireless_anonymous@protonmail.com>
2022-03-28 21:56:28 -0400

    add redirect to official site

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: 'https://refund.cashio.app/',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/create',
+        destination: 'https://refund.cashio.app/create',
+        permanent: true,
+        basePath: false,
+      },
+    ]
+  },
 }
